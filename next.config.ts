efa5e79466ad712dbf7bdaf8dd7b8222c61b85f8
ziancn <ziancn@hotmail.com>
2026-06-05 22:50:37 +0800

    Setup static export and GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
         "192.168.0.*",     // 匹配 192.168.0.X 的所有局域网设备
         "192.168.0.243"    // 明确写死当前 IP 也可以
     ],
+
+    output: 'export', // 启用静态导出
+    images: {
+        unoptimized: true, // GitHub Pages 不支持 Next.js 默认的图片优化，必须关闭
+    },
+    trailingSlash: true, // 可选：如果遇到路由刷新 404，可以开启此项
 };
 
 export default nextConfig;
